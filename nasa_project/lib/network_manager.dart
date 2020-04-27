d8bacdf089f1eea022f4f6d54a3ccdfe29b7e49b
Bernardino Guerrero Ibarra <bernardino.guerrero@gmail.com>
2020-04-26 23:16:38 -0500

    refactor getImagery by fetchImagery to return a List of Imagery

--- nasa_project/lib/network_manager.dart
+++ nasa_project/lib/network_manager.dart
@@ -1,14 +1,17 @@
 import 'dart:convert';
+import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Imagery>> getImagery() async {
-  final response = await http.get('https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=gsfEXbNnWakbyHncc562Xz5Bwsm2XEcBiZ2K3mNo');
-  if (response.statusCode == 200) {
-    return Imagery.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load Imagery');
-  }
+Future<List<Imagery>> fetchImagery(http.Client client) async {
+  final response = await client.get('https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=gsfEXbNnWakbyHncc562Xz5Bwsm2XEcBiZ2K3mNo');
+  return compute(parseImagery, response.body);
+}
+
+List<Imagery> parseImagery(String responseBody) {
+  final parse = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parse.map<Imagery>((json) => Imagery.fromJson(json)).toList();
 }
 
 class Imagery {
@@ -17,7 +20,7 @@ class Imagery {
   final String image;
   final String date;
 
-  Imagery({this.identifier, this.caption, this.image, this.date})
+  Imagery({this.identifier, this.caption, this.image, this.date});
 
   factory Imagery.fromJson(Map<String, dynamic> json) {
     return Imagery(
